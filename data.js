e7bf2dc63cfca876c62078d6771e71530e9a5dcc
Superc00kie <victorialecoutere@gmail.com>
2025-08-27 16:20:52 +0200

    functional search

--- data.js
+++ data.js
@@ -2,20 +2,23 @@ const collection=[
     {
         "type":"book",
         "title":"Cecilia",
+        "author":"Dr Seuss",
         "length_pages":27,
         "themes":["memory"],
         "link":"./works/Cecilia.html"
     },
     {
         "type":"book",
-        "title":"book2",
+        "title":"Book2",
+        "author":"Dr Seuss",
         "length_pages":350,
         "themes":["cats"],
         "link":"./works/Book2.html"
     },
     {
         "type":"book",
-        "title":"story",
+        "title":"Story",
+        "author":"Dr Seuss",
         "length_pages":100,
         "themes":["happy_stuff","cats"],
         "link":"./works/Cecilia.html"
--- script.js
+++ script.js
@@ -2,25 +2,24 @@ function onload(){
 document.getElementById("plength").oninput = function() {
   document.getElementById("prange").innerHTML = "Length in pages: 0 to "+this.value;
 }
-document.getElementById("tlength").oninput = function() {
-  document.getElementById("trange").innerHTML = "Length in minutes: 0 to "+this.value;
-}
 parseform()
 }
 function parseform(){
     l=document.getElementById('plength').value;
+    s=document.getElementById('search').value
     options=document.getElementsByClassName("themes");
     t=[];
     for (const obj of options){
         if (obj.checked){t.push(obj.id)}
     }
-    console.log(l,t);
-    generate(filter(l,t));
+    //console.log(l,t);
+    generate(filter(l,t,s));
     
 }
-function filter(l,t){
-    arr=[]
-    for (const obj of collection){
+function filter(l,t,s){
+    var arr=[]
+    searched_col=search(s)
+    for (const obj of searched_col){
         if ((obj.length_pages<=l )&& (obj.themes.some(item => t.includes(item)))){
             arr.push(obj)
         }
@@ -34,4 +33,51 @@ function generate(books){
         txt+= "<tr><td><a href='" + obj.link + "'>"+ obj.title+"</td><td>" + obj.length_pages + "</td><td>" + obj.themes + "</td>"
     }
     document.getElementById("table").innerHTML=txt
+}
+function search(string){
+    if (string==""){
+        return collection;
+    }
+    var arr=[]
+    for (const obj of collection){
+        if ((obj.title.includes(capitalize(string)))||(obj.author.includes(capitalize(string)))){
+            arr.push(obj);
+            continue
+        }
+        else {
+            for (const theme of obj.themes){
+                if (theme.includes(capitalize(string))){
+                    arr.push(obj);
+                    continue;
+                }
+            }
+        }
+        if ((obj.title.includes((string)))||(obj.author.includes((string)))){
+            arr.push(obj);
+        }
+        else {
+            for (const theme of obj.themes){
+                if (theme.includes((string))){
+                    arr.push(obj);
+                    continue;
+                }
+            }
+        }
+    }
+    return arr
+}
+function capitalize(word){
+    return word.charAt(0).toUpperCase()
+  + word.slice(1)
+}
+
+function clearfilters(){
+    for (const obj of document.getElementsByClassName("themes")){
+        obj.checked=false
+    }
+}
+function checkfilters(){
+    for (const obj of document.getElementsByClassName("themes")){
+        obj.checked=true
+    }
 }
